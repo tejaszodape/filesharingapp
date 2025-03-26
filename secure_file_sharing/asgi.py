92dc19a1542556c660ee8026d0c55db87a23083e
tejaszodape <tejasz.cse22@sbjit.edu.in>
2025-03-27 01:32:26 +0530

    update

--- secure_file_sharing/asgi.py
+++ secure_file_sharing/asgi.py
@@ -7,23 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
-
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+# Ensure settings are set before running anything
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "secure_file_sharing.settings")
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from file_sharing  import routing  
+from file_sharing import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secure_file_sharing.settings')
-
-
-# Initialize the ASGI application
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(routing.websocket_urlpatterns)
+        ),
+    }
+)
