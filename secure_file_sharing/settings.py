5662fb883ff700b8635fee45a2cf51b447ae4c9d
tejaszodape <tejasz.cse22@sbjit.edu.in>
2025-03-27 01:29:44 +0530

    update

--- secure_file_sharing/settings.py
+++ secure_file_sharing/settings.py
@@ -47,14 +47,14 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'daphne',
     'channels',
-
+    'file_sharing',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'file_sharing',
+    
 
 ]
 
